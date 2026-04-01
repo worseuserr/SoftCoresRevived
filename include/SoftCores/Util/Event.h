09c505a1eebd180e63e520437740e450f34578dc
worse <oskar.rabo@gmail.com>
2026-04-01 02:35:57 +0200

    Implement event class

--- include/SoftCores/Util/Event.h
+++ include/SoftCores/Util/Event.h
@@ -3,6 +3,8 @@
 
 namespace SoftCores::Util
 {
+	using NO_SENDER = void*;
+
 	template	<typename Sender, typename Value>
 	// An event implementation that does not rely on inheritance.
 	// Con: Type safety and defining value names.
@@ -10,11 +12,27 @@ namespace SoftCores::Util
 	class Event
 	{
 	protected:
-		std::vector<void *(Sender, Value)>	Listeners_;
+		std::vector<void (*)(Sender, Value)>	Listeners_;
 	public:
-		Event<Sender, Value>();
-		void		Dispatch(Sender sender, Value value);
-		Event		&operator+=(void (*func)(Sender sender, Value value));
-		Event		&operator-=(void (*func)(Sender sender, Value value));
+		Event() = default;
+		void		Dispatch(Sender sender, Value value)
+		{
+			for (auto func : Listeners_)
+			{
+				func(sender, value);
+			}
+		}
+
+		Event		&operator+=(void (*func)(Sender sender, Value value))
+		{
+			Listeners_.push_back(func);
+			return (this);
+		}
+
+		Event		&operator-=(void (*func)(Sender sender, Value value))
+		{
+			std::erase(Listeners_, func);
+			return (this);
+		}
 	};
 }
