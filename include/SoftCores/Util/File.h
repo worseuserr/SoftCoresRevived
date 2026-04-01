1ee59ff25368ff47087c3ea23a7189f87cb4a30d
worse <oskar.rabo@gmail.com>
2026-04-01 02:33:26 +0200

    Add WideToUTF8 method and remove file underscore suffix

--- include/SoftCores/Util/File.h
+++ include/SoftCores/Util/File.h
@@ -6,17 +6,18 @@ namespace SoftCores::Util
 	class File
 	{
 	private:
-		std::ofstream	file_;
+		std::ofstream	file;
 
 	public:
-		void		Open(const char *filename, std::ios_base::openmode mode);
-		void		Close();
-		bool		IsOpen() const;
-		static bool	Exists(const char *filename);
+		void				Open(const char *filename, std::ios_base::openmode mode);
+		void				Close();
+		bool				IsOpen() const;
+		static bool			Exists(const char *filename);
+		static std::string	WideToUTF8(const wchar_t *wstr);
 		template	<typename T>
 		File&		operator<<(const T& data)
 		{
-			file_ << data;
+			file << data;
 			return (*this);
 		}
 	};
