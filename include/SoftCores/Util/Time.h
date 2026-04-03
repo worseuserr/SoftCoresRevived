9e2e175f3394ffd8442c86edc03a2c8aa9907a22
worse <oskar.rabo@gmail.com>
2026-04-03 05:28:19 +0200

    Add documentation to HasDurationPassed

--- include/SoftCores/Util/Time.h
+++ include/SoftCores/Util/Time.h
@@ -2,5 +2,6 @@
 
 namespace SoftCores::Util
 {
+	// Returns true after every <milliseconds> milliseconds pass, otherwise false. Updates counter on every true. Counter must be initialized.
 	bool	HasDurationPassed(const unsigned long long &milliseconds, unsigned long long *counter);
 }
