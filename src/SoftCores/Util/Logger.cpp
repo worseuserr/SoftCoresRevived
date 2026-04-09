8905beca9fb3ddc0573a0954f91bae12908a8758
worse <oskar.rabo@gmail.com>
2026-04-09 05:41:38 +0200

    Remove duplicated code from wchar_t Write and format string properly

--- src/SoftCores/Util/Logger.cpp
+++ src/SoftCores/Util/Logger.cpp
@@ -1,6 +1,7 @@
 #include <SoftCores/Util/File.h>
 #include <SoftCores/Util/Logger.h>
 #include <sstream>
+#include <format>
 
 using namespace SoftCores::Util;
 
@@ -9,16 +10,6 @@ Logger::Logger(const char *outputFilename)
 	OutputFile = outputFilename;
 	File.Open(outputFilename, std::ios_base::out);
 	File.Close();
-}
-
-void Logger::Write(const std::string &message)
-{
-	return (Write(message.c_str()));
-}
-
-void Logger::Write(const std::wstring &message)
-{
-	return (Write(message.c_str()));
 }
 
 void Logger::Write(const char *message)
@@ -33,38 +24,30 @@ void Logger::Write(const char *message)
 	File.Open(OutputFile, std::ios_base::app);
 	if (File.IsOpen())
 	{
-		text << "[" << newtime.tm_mday
-			<< "/" << newtime.tm_mon + 1
-			<< "/" << newtime.tm_year + 1900
-			<< " " << newtime.tm_hour
-			<< ":" << newtime.tm_min
-			<< ":" << newtime.tm_sec
-			<< "] " << message;
+		text << std::format("[{:02}/{:02}/{} {:02}:{:02}:{:02}] {}",
+			newtime.tm_mday,
+			newtime.tm_mon + 1,
+			newtime.tm_year + 1900,
+			newtime.tm_hour,
+			newtime.tm_min,
+			newtime.tm_sec,
+			message);
 		File << text.str().c_str() << "\n";
 		File.Close();
 	}
 }
 
+void Logger::Write(const std::string &message)
+{
+	return (Write(message.c_str()));
+}
+
+void Logger::Write(const std::wstring &message)
+{
+	return (Write(message.c_str()));
+}
+
 void Logger::Write(const wchar_t *message)
 {
-	tm					newtime{};
-	time_t				now;
-	std::stringstream	text;
-
-	now = time(nullptr);
-	if (localtime_s(&newtime, &now) != 0)
-		throw (std::exception());
-	File.Open(OutputFile, std::ios_base::app);
-	if (File.IsOpen())
-	{
-		text << "[" << newtime.tm_mday
-			<< "/" << newtime.tm_mon + 1
-			<< "/" << newtime.tm_year + 1900
-			<< " " << newtime.tm_hour
-			<< ":" << newtime.tm_min
-			<< ":" << newtime.tm_sec
-			<< "] " << File::WideToUTF8(message);
-		File << text.str().c_str() << "\n";
-		File.Close();
-	}
+	return (Write(File::WideToUTF8(message).c_str()));
 }
