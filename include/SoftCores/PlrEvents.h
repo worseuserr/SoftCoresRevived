4a37caa3406d81fef1d45812d0d7c5392d14ae0a
worse <oskar.rabo@gmail.com>
2026-04-16 14:06:06 +0200

    Add OnDeadeyeChanged event

--- include/SoftCores/PlrEvents.h
+++ include/SoftCores/PlrEvents.h
@@ -15,15 +15,17 @@ namespace SoftCores
 		Util::ChangedEvent<PlrEvents, bool>	OnControlChanged = false;
 		Util::ChangedEvent<PlrEvents, bool>	OnMovingChanged = false;
 		Util::ChangedEvent<PlrEvents, bool>	OnBathingChanged = false;
+		Util::ChangedEvent<PlrEvents, bool>	OnDeadeyeChanged = false;
 		Util::ChangedEvent<PlrEvents, int>	OnDeadeyeCoreChanged = 0;
 		Util::ChangedEvent<PlrEvents, int>	OnStaminaCoreChanged = 0;
 
-		void FTick(Util::NO_SENDER _, float dTime)
+		void FTick(Util::NO_SENDER _, const float dTime)
 		{
 			OnSleepingChanged.Update(this, Plr::IsInSleepScenario());
 			OnControlChanged.Update(this, Plr::IsInControl());
 			OnBathingChanged.Update(this, Plr::IsBathing());
 			OnMovingChanged.Update(this, Plr::IsMoving());
+			OnDeadeyeChanged.Update(this, Plr::IsDeadeyeActivated());
 			OnDeadeyeCoreChanged.Update(this, Plr::GetCore(Core::Deadeye));
 			OnStaminaCoreChanged.Update(this, Plr::GetCore(Core::Stamina));
 		}
